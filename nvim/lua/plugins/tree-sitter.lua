1fca8e6a90acc6072bcedfff9a48fcc88e879926
Jean-Paul Grandgirard <jp.grandgirard@protonmail.com>
2024-03-10 20:00:36 +0100

    [nvim] add yaml to tree-sitter

--- nvim/lua/plugins/tree-sitter.lua
+++ nvim/lua/plugins/tree-sitter.lua
@@ -4,7 +4,7 @@ return {
     build = ":TSUpdate",
     config = function()
       require('nvim-treesitter.configs').setup({
-        ensure_installed = { "c", "lua", "rust", "toml", "bash", "cpp", "css", "vim", "vimdoc", "javascript", "html", "xml" },
+        ensure_installed = { "c", "lua", "rust", "toml", "bash", "cpp", "css", "vim", "vimdoc", "javascript", "html", "xml", "yaml" },
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true },
